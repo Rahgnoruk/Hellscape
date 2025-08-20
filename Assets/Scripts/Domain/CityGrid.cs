651f9f1c5e9d69f5854c36aa64106936f2b816bc
Tony Raimond <tono.raimond@hotmail.com>
2025-08-19 19:23:26 -0600

    Enforce Domain in CityGrid
    We will use system math instead of Unity math so Domain doesn't reference Unity modules

--- Assets/Scripts/Domain/CityGrid.cs
+++ Assets/Scripts/Domain/CityGrid.cs
@@ -1,36 +1,35 @@
-using UnityEngine;
-
-
-namespace Hellscape.World {
+namespace Hellscape.Domain {
     public enum TileTag : byte { Suburbs, Industrial, Downtown, Park, Blocked }
 
-
     public sealed class CityGrid {
-        public readonly int Width, Height; public readonly int CenterX, CenterY; public readonly int CenterRadius;
+        public readonly int Width, Height; 
+        public readonly int CenterX, CenterY; 
+        public readonly int CenterRadius;
         private readonly TileTag[,] tags;
 
-
         public CityGrid(int width, int height, int centerRadius) {
-            Width = width; Height = height; CenterRadius = centerRadius;
-            CenterX = width/2; CenterY = height/2; tags = new TileTag[width, height];
+            Width = width; 
+            Height = height; 
+            CenterRadius = centerRadius;
+            CenterX = width / 2; 
+            CenterY = height / 2; 
+            tags = new TileTag[width, height];
             Generate();
         }
 
+        public TileTag GetTag(int x, int y) => tags[x, y];
 
-        public TileTag GetTag(int x,int y) => tags[x,y];
-
-
-        public int RadiusIndex(int x,int y){ 
-            int dx=x-CenterX, dy=y-CenterY; 
-            return Mathf.CeilToInt(Mathf.Max(Mathf.Abs(dx), Mathf.Abs(dy))); 
+        public int RadiusIndex(int x, int y) { 
+            int dx = x - CenterX, dy = y - CenterY; 
+            return System.Math.Max(System.Math.Abs(dx), System.Math.Abs(dy)); 
         }
 
-
-        private void Generate(){
-            for(int x=0;x<Width;x++){
-                for(int y=0;y<Height;y++){
-                    var r = RadiusIndex(x,y);
-                    tags[x,y] = r < CenterRadius*0.5f ? TileTag.Downtown : r < CenterRadius ? TileTag.Industrial : TileTag.Suburbs;
+        private void Generate() {
+            for (int x = 0; x < Width; x++) {
+                for (int y = 0; y < Height; y++) {
+                    var r = RadiusIndex(x, y);
+                    tags[x, y] = r < CenterRadius * 0.5f ? TileTag.Downtown : 
+                                r < CenterRadius ? TileTag.Industrial : TileTag.Suburbs;
                 }
             }
         }
