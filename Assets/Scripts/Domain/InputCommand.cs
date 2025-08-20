96a5c5f4c0dee19c8a59be365ac347937aebf538
Tony Raimond <tono.raimond@hotmail.com>
2025-08-20 01:49:29 -0600

    Created Input Command
    - It's the output we use to communicate all inputs. The interpreted version we can consume in the rest of our code.

---
+++ Assets/Scripts/Domain/InputCommand.cs
@@ -0,0 +1,17 @@
+namespace Hellscape.Domain {
+    public struct InputCommand {
+        public int tick;
+        public float moveX, moveY;
+        public float aimX, aimY;
+        public byte buttons;
+        
+        public InputCommand(int tick, float moveX, float moveY, float aimX, float aimY, byte buttons) {
+            this.tick = tick;
+            this.moveX = moveX;
+            this.moveY = moveY;
+            this.aimX = aimX;
+            this.aimY = aimY;
+            this.buttons = buttons;
+        }
+    }
+}
